d5a863ab4aee121013fea2131830338a87f49de6
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-13 20:53:00 +0200

    day 1. aritmetics solutions

---
+++ 01_Day_Introduction/helloworld2.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+division_entera = a // b
+resto = a % b
+potencia = a ** b
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("División entera:", division_entera)
+print("Resto:", modulo)
+print("Potencia:", potencia)
